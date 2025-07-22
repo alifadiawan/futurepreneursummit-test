93fab3587769bb0070bf4e277e481268a2b3eff1
alifadiawan <alifadiawan2005@gmail.com>
2025-07-22 14:26:56 +0700

    optimize

--- src/Components/PixelTransition.jsx
+++ src/Components/PixelTransition.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
+import { motion } from "framer-motion";
 
 const PixelTransition = ({ imageUrl, title, ctaText, ctaLink }) => {
   return (
-    <div className="max-w-sm mx-auto overflow-hidden rounded-2xl border shadow-lg border-orange-200 transition-all hover:-translate-y-3.5">
-      {/* 1. Image */}
-      <img
-        loading='lazy'
-        src={imageUrl}
-        alt={title}
-        className="w-full  object-cover"
-        onError={(e) => {
-          e.target.onerror = null;
-          e.target.src = 'https://placehold.co/600x400/000000/FFFFFF?text=Image+Not+Found';
-        }}
-      />
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.1, ease: "easeOut" }}
+      whileHover={{ scale: 1.05 }}
+      className="max-w-sm mx-auto overflow-hidden rounded-2xl border border-orange-200 shadow-lg transition-transform"
+    >
+      <div className="aspect-[16/9] w-full">
+        <img
+          src={imageUrl}
+          alt={title}
+          decoding="async"
+          loading="lazy"
+          fetchpriority="low"
+          width="640"
+          height="360"
+          className="w-full h-full object-contain"
+          onError={(e) => {
+            e.target.onerror = null;
+            e.target.src = 'https://placehold.co/640x360/000000/FFFFFF?text=Image+Not+Found';
+          }}
+        />
+      </div>
+    </motion.div>
   )
 }
 
